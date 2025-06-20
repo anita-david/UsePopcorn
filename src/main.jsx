7480e33eb1c7335e5fc8fbda79d6b36a7a2b407e
Anita David <anitafunmilayo@gmail.com>
2025-06-20 11:58:09 +0100

    Fixed form event handler issue

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,31 @@
-import { StrictMode } from "react";
+import { StrictMode, useState } from "react";
 import { createRoot } from "react-dom/client";
 // import './index.css'
 // import App from './App.jsx'
 import StarRating from "./StarRating.jsx";
 
+function Test() {
+  const [rated, setRated] = useState(0);
+  return (
+    <div>
+      <StarRating
+        color="purple"
+        setRate={setRated}
+        message={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      />
+      <p>You rated this movie {rated} stars</p>
+    </div>
+  );
+}
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <StarRating maxRating={5} />
-    {/* <StarRating maxRating={10} />
-    <StarRating /> */}
+    <StarRating
+      defaultRating={2}
+      message={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating />
+    <Test />
   </StrictMode>
 );
